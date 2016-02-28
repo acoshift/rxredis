d045f522d235a3adc4e42d1c2bfbf7f1adc756c6
Thanatat Tamtan <acoshift@gmail.com>
2016-02-29 00:32:46 +0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# rxredis
-ioredis wrapped with rxjs
+# RxRedis
+RxJS wrapper for Redis
+
+### Example
+```js
+'use strict'
+const Observable = require('rxjs').Observable
+const Redis = require('ioredis')
+const client = require('rxredis')(new Redis())
+
+client
+  .incr('test')
+  .flatMap((id) => client.hmset(`test:${id}`, 'name', 'abc', 'value', Math.floor(Math.random() * 20)), (id) => id)
+  .flatMap((id) => client.sadd('test:all', id), (id) => id)
+  .flatMap((id) => client.hgetall(`test:${id}`))
+  .repeat(10)
+  .subscribe(console.log, console.error, () => console.log('1 Completed!'))
+
+client
+  .smembers('test:all')
+  .flatMap(Observable.fromArray)
+  .flatMap((id) => client.hgetall(`test:${id}`))
+  .toArray()
+  .subscribe(console.log, console.error, () => console.log('2 Completed!'))
+
+client
+  .pipeline()
+  .hgetall('test:5')
+  .sismember('test:all', 5)
+  .get('test')
+  .exec()
+  .map((xs) => {
+    let x = xs[0]
+    x.id = 5
+    x.isMember = xs[1]
+    x.last = xs[2]
+    return x
+  })
+  .subscribe(console.log, console.error, () => console.log('3 Completed!'))
+```
--- index.js
+++ index.js
@@ -4,15 +4,35 @@ function rxredis (client) {
   var obj = {}
   obj.__client = client
 
+  /* Connection */
+  obj.auth = Observable.bindNodeCallback(obj.__client.auth.bind(obj.__client))
+  obj.echo = Observable.bindNodeCallback(obj.__client.echo.bind(obj.__client))
+  obj.ping = Observable.bindNodeCallback(obj.__client.ping.bind(obj.__client))
+  obj.quit = Observable.bindNodeCallback(obj.__client.quit.bind(obj.__client))
+  obj.select = Observable.bindNodeCallback(obj.__client.select.bind(obj.__client))
+
   /* Keys */
   obj.del = Observable.bindNodeCallback(obj.__client.del.bind(obj.__client))
   obj.dump = Observable.bindNodeCallback(obj.__client.dump.bind(obj.__client))
   obj.exists = Observable.bindNodeCallback(obj.__client.exists.bind(obj.__client))
   obj.expire = Observable.bindNodeCallback(obj.__client.expire.bind(obj.__client))
   obj.expireat = Observable.bindNodeCallback(obj.__client.expireat.bind(obj.__client))
+  obj.keys = Observable.bindNodeCallback(obj.__client.keys.bind(obj.__client))
+  obj.migrate = Observable.bindNodeCallback(obj.__client.migrate.bind(obj.__client))
   obj.move = Observable.bindNodeCallback(obj.__client.move.bind(obj.__client))
+  obj.object = Observable.bindNodeCallback(obj.__client.object.bind(obj.__client))
+  obj.persist = Observable.bindNodeCallback(obj.__client.persist.bind(obj.__client))
+  obj.pexpire = Observable.bindNodeCallback(obj.__client.pexpire.bind(obj.__client))
+  obj.pexpireat = Observable.bindNodeCallback(obj.__client.pexpireat.bind(obj.__client))
+  obj.pttl = Observable.bindNodeCallback(obj.__client.pttl.bind(obj.__client))
+  obj.randomkey = Observable.bindNodeCallback(obj.__client.randomkey.bind(obj.__client))
   obj.rename = Observable.bindNodeCallback(obj.__client.rename.bind(obj.__client))
   obj.renamenx = Observable.bindNodeCallback(obj.__client.renamenx.bind(obj.__client))
+  obj.restore = Observable.bindNodeCallback(obj.__client.restore.bind(obj.__client))
+  obj.sort = Observable.bindNodeCallback(obj.__client.sort.bind(obj.__client))
+  obj.ttl = Observable.bindNodeCallback(obj.__client.ttl.bind(obj.__client))
+  obj.type = Observable.bindNodeCallback(obj.__client.type.bind(obj.__client))
+  obj.wait = Observable.bindNodeCallback(obj.__client.wait.bind(obj.__client))
   obj.scan = Observable.bindNodeCallback(obj.__client.scan.bind(obj.__client))
 
   /* Strings */
@@ -116,8 +136,58 @@ function rxredis (client) {
   obj.zunionstore = Observable.bindNodeCallback(obj.__client.zunionstore.bind(obj.__client))
   obj.zscan = Observable.bindNodeCallback(obj.__client.zscan.bind(obj.__client))
 
+  /* HyperLogLog */
+  obj.pfadd = Observable.bindNodeCallback(obj.__client.pfadd.bind(obj.__client))
+  obj.pfcount = Observable.bindNodeCallback(obj.__client.pfcount.bind(obj.__client))
+  obj.pfmerge = Observable.bindNodeCallback(obj.__client.pfmerge.bind(obj.__client))
+
+  /* Pub/Sub */
+  obj.psubscribe = Observable.bindNodeCallback(obj.__client.psubscribe.bind(obj.__client))
+  obj.pubsub = Observable.bindNodeCallback(obj.__client.pubsub.bind(obj.__client))
+  obj.publish = Observable.bindNodeCallback(obj.__client.publish.bind(obj.__client))
+  obj.punsubscribe = Observable.bindNodeCallback(obj.__client.punsubscribe.bind(obj.__client))
+  obj.subscribe = Observable.bindNodeCallback(obj.__client.subscribe.bind(obj.__client))
+  obj.unsubscribe = Observable.bindNodeCallback(obj.__client.unsubscribe.bind(obj.__client))
+
+  /* Scripting */
   obj.eval = Observable.bindNodeCallback(obj.__client.eval.bind(obj.__client))
   obj.evalsha = Observable.bindNodeCallback(obj.__client.evalsha.bind(obj.__client))
+  obj.script = Observable.bindNodeCallback(obj.__client.script.bind(obj.__client))
+
+  /* Server */
+  obj.bgrewriteaof = Observable.bindNodeCallback(obj.__client.bgrewriteaof.bind(obj.__client))
+  obj.bgsave = Observable.bindNodeCallback(obj.__client.bgsave.bind(obj.__client))
+  obj.client = Observable.bindNodeCallback(obj.__client.client.bind(obj.__client))
+  obj.command = Observable.bindNodeCallback(obj.__client.command.bind(obj.__client))
+  obj.config = Observable.bindNodeCallback(obj.__client.config.bind(obj.__client))
+  obj.dbsize = Observable.bindNodeCallback(obj.__client.dbsize.bind(obj.__client))
+  obj.debug = Observable.bindNodeCallback(obj.__client.debug.bind(obj.__client))
+  obj.flushall = Observable.bindNodeCallback(obj.__client.flushall.bind(obj.__client))
+  obj.flushdb = Observable.bindNodeCallback(obj.__client.flushdb.bind(obj.__client))
+  obj.info = Observable.bindNodeCallback(obj.__client.info.bind(obj.__client))
+  obj.lastsave = Observable.bindNodeCallback(obj.__client.lastsave.bind(obj.__client))
+  obj.monitor = Observable.bindNodeCallback(obj.__client.monitor.bind(obj.__client))
+  obj.role = Observable.bindNodeCallback(obj.__client.role.bind(obj.__client))
+  obj.save = Observable.bindNodeCallback(obj.__client.save.bind(obj.__client))
+  obj.shutdown = Observable.bindNodeCallback(obj.__client.shutdown.bind(obj.__client))
+  obj.slaveof = Observable.bindNodeCallback(obj.__client.slaveof.bind(obj.__client))
+  obj.slowlog = Observable.bindNodeCallback(obj.__client.slowlog.bind(obj.__client))
+  obj.sync = Observable.bindNodeCallback(obj.__client.sync.bind(obj.__client))
+  obj.time = Observable.bindNodeCallback(obj.__client.time.bind(obj.__client))
+
+  /* Transactions */
+  obj.discard = Observable.bindNodeCallback(obj.__client.discard.bind(obj.__client))
+
+  obj.exec = function () {
+    return Observable
+      .bindNodeCallback(obj.__client.exec.bind(obj.__client))()
+      .map(function (xs) {
+        return xs.map(function (x) {
+          if (x[0]) throw new Error(x[0])
+          return x[1]
+        })
+      })
+  }
 
   obj.multi = function () {
     var p = obj.__client.multi()
@@ -150,6 +220,15 @@ function rxredis (client) {
     }
     return p
   }
+
+  obj.unwatch = Observable.bindNodeCallback(obj.__client.unwatch.bind(obj.__client))
+  obj.watch = Observable.bindNodeCallback(obj.__client.watch.bind(obj.__client))
+
+  /* Cluster */
+  obj.cluster = Observable.bindNodeCallback(obj.__client.cluster.bind(obj.__client))
+  obj.readonly = Observable.bindNodeCallback(obj.__client.readonly.bind(obj.__client))
+  obj.readwrite = Observable.bindNodeCallback(obj.__client.readwrite.bind(obj.__client))
+
 
   return obj
 }
